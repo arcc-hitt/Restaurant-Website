2ca9e602df5a4097199d9d947e75374185b48581
Archit Mahule <archit.mahule@mathongo.com>
2025-08-18 17:20:12 +0530

    add meals summary

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import { Fragment } from 'react';
 
 import Header from './components/Layout/Header';
+import MealsSummary from './components/Meals/MealsSummary';
 
 function App() {
   return (
     <Fragment>
       <Header />
+      <main>
+        <MealsSummary />
+      </main>
     </Fragment>
   );
 }
